b0d7408b8bc1caee6f6a6e16de432e61a25d5b0b
luobl <luobl@asiacom-online.com>
2014-09-10 18:42:49 +0800

    修复高度设置wrap_content显示不全的bug

--- readme.md
+++ readme.md
@@ -23,7 +23,7 @@
 
 ##问题
 
-- `CardView`高度设置为`wrap_content`卡片会显示不全。
+- `CardView`高度设置为`wrap_content`卡片会显示不全。(已修复)
 - `CardView`设置`padding`和`margin`，旋转动画会有被截断的效果。最后一张卡片距离顶部的距离是在CardView内部指定的，为固定值。
 - 测试2.3.2有严重bug（估计4.0版本之前都有此bug），不可用。
 
--- src/com/chiemy/cardview/view/CardView.java
+++ src/com/chiemy/cardview/view/CardView.java
@@ -1,19 +1,20 @@
 package com.chiemy.cardview.view;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.database.DataSetObserver;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.view.View.MeasureSpec;
 import android.view.animation.AccelerateInterpolator;
 import android.widget.FrameLayout;
 import android.widget.ListAdapter;
 
-import com.chiemy.cardview.Utils;
 import com.nineoldandroids.animation.Animator;
 import com.nineoldandroids.animation.AnimatorListenerAdapter;
 import com.nineoldandroids.view.ViewHelper;
@@ -21,16 +22,15 @@ import com.nineoldandroids.view.ViewPropertyAnimator;
 
 /**
  * @author chiemy
- *
+ * 
  */
-public class CardView extends FrameLayout{
+public class CardView extends FrameLayout {
 	private static final int ITEM_SPACE = 40;
 	private static final int DEF_MAX_VISIBLE = 4;
-	private static final int PADDING_TOP = 20;
-	
+
 	private int mMaxVisible = DEF_MAX_VISIBLE;
 	private int itemSpace = ITEM_SPACE;
-	
+
 	private float mTouchSlop;
 	private ListAdapter mListAdapter;
 	private int mNextAdapterPosition;
@@ -38,11 +38,11 @@ public class CardView extends FrameLayout{
 	private OnCardClickListener mListener;
 	private int topPosition;
 	private Rect topRect;
-	
-	public interface OnCardClickListener{
-		void onCardClick(View view,int position);
+
+	public interface OnCardClickListener {
+		void onCardClick(View view, int position);
 	}
-	
+
 	public CardView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		init();
@@ -57,35 +57,35 @@ public class CardView extends FrameLayout{
 		super(context);
 		init();
 	}
-	
+
 	private void init() {
 		topRect = new Rect();
 		ViewConfiguration con = ViewConfiguration.get(getContext());
 		mTouchSlop = con.getScaledTouchSlop();
 	}
-	
+
 	public void setMaxVisibleCount(int count) {
 		mMaxVisible = count;
 	}
-	
+
 	public int getMaxVisibleCount() {
-		return mMaxVisible; 
+		return mMaxVisible;
 	}
-	
+
 	public void setItemSpace(int itemSpace) {
 		this.itemSpace = itemSpace;
 	}
-	
+
 	public int getItemSpace() {
 		return itemSpace;
 	}
-	
+
 	public ListAdapter getAdapter() {
 		return mListAdapter;
 	}
-	
+
 	public void setAdapter(ListAdapter adapter) {
-		if (mListAdapter != null){
+		if (mListAdapter != null) {
 			mListAdapter.unregisterDataSetObserver(mDataSetObserver);
 		}
 		mNextAdapterPosition = 0;
@@ -94,56 +94,93 @@ public class CardView extends FrameLayout{
 		removeAllViews();
 		ensureFull();
 	}
-	
+
 	public void setOnCardClickListener(OnCardClickListener listener) {
 		mListener = listener;
 	}
-	
+
 	private void ensureFull() {
 		while (mNextAdapterPosition < mListAdapter.getCount()
 				&& getChildCount() < mMaxVisible) {
 			int index = mNextAdapterPosition % mMaxVisible;
 			View convertView = viewHolder.get(index);
-			final View view = mListAdapter.getView(mNextAdapterPosition, convertView, this);
+			final View view = mListAdapter.getView(mNextAdapterPosition,
+					convertView, this);
 			view.setOnClickListener(null);
+			// view.setOnTouchListener(null);
 			viewHolder.put(index, view);
-			
-			//添加剩余的View时，始终处在最后
+
+			// 添加剩余的View时，始终处在最后
 			index = Math.min(mNextAdapterPosition, mMaxVisible - 1);
-			ViewHelper.setScaleX(view, ((mMaxVisible - index - 1)/(float)mMaxVisible)*0.2f + 0.8f);
-			int topMargin = (mMaxVisible - index - 1)*itemSpace + PADDING_TOP;
+			ViewHelper.setScaleX(view,((mMaxVisible - index - 1) / (float) mMaxVisible) * 0.2f + 0.8f);
+			int topMargin = (mMaxVisible - index - 1) * itemSpace;
 			ViewHelper.setTranslationY(view, topMargin);
 			ViewHelper.setAlpha(view, mNextAdapterPosition == 0 ? 1 : 0.5f);
-			
+
 			LayoutParams params = (LayoutParams) view.getLayoutParams();
-			if(params == null){
-				params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+			if (params == null) {
+				params = new LayoutParams(LayoutParams.MATCH_PARENT,
+						LayoutParams.WRAP_CONTENT);
 			}
-			addViewInLayout(view,0, params, true);
-			
+			addViewInLayout(view, 0, params);
+
 			mNextAdapterPosition += 1;
 		}
-		requestLayout();
+		// requestLayout();
 	}
-	
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+		int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+		int childCount = getChildCount();
+		int maxHeight = 0;
+		int maxWidth = 0;
+		for (int i = 0; i < childCount; i++) {
+			View child = getChildAt(i);
+			this.measureChild(child, widthMeasureSpec, heightMeasureSpec);
+			int height = child.getMeasuredHeight();
+			int width = child.getMeasuredWidth();
+			if (height > maxHeight) {
+				maxHeight = height;
+			}
+			if (width > maxWidth) {
+				maxWidth = width;
+			}
+		}
+		int desireWidth = widthSize;
+		int desireHeight = heightSize;
+		if (widthMode == MeasureSpec.AT_MOST) {
+			desireWidth = maxWidth + getPaddingLeft() + getPaddingRight();
+		}
+		if (heightMode == MeasureSpec.AT_MOST) {
+			desireHeight = maxHeight + (mMaxVisible - 1) * itemSpace + getPaddingTop() + getPaddingBottom();
+		}
+		setMeasuredDimension(desireWidth, desireHeight);
+	}
+
 	@Override
 	protected void onLayout(boolean changed, int left, int top, int right,
 			int bottom) {
 		super.onLayout(changed, left, top, right, bottom);
-		View topView = getChildAt(getChildCount()-1);
-		if(topView != null){
+		View topView = getChildAt(getChildCount() - 1);
+		if (topView != null) {
 			topView.setOnClickListener(listener);
 		}
 	}
-	
-	float downX,downY;
+
+	float downX, downY;
 	boolean remove = false;
+
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		switch(event.getAction()){
+		switch (event.getAction()) {
 		case MotionEvent.ACTION_MOVE:
-			if(!remove){
-				if(goDown()){
+			if (!remove) {
+				if (goDown()) {
 					downY = -1;
 					remove = true;
 				}
@@ -152,42 +189,48 @@ public class CardView extends FrameLayout{
 		}
 		return super.onTouchEvent(event);
 	}
-	
+
 	/**
 	 * 下移所有视图
 	 */
 	private boolean goDown() {
 		final View topView = getChildAt(getChildCount() - 1);
-		//topView.getHitRect(topRect); 在4.3以前有bug，用以下方法代替
+		// topView.getHitRect(topRect); 在4.3以前有bug，用以下方法代替
 		topRect = getHitRect(topRect, topView);
-		//如果按下的位置不在顶部视图上，则不移动
-		if(!topRect.contains((int)downX, (int)downY)){
+		// 如果按下的位置不在顶部视图上，则不移动
+		if (!topRect.contains((int) downX, (int) downY)) {
 			return false;
 		}
-		ViewPropertyAnimator anim = ViewPropertyAnimator.animate(topView)
-				.translationY(ViewHelper.getTranslationY(topView) + topView.getHeight())
-				.alpha(0).scaleX(1)
-				.setListener(null)
-				.setDuration(200);
+		ViewPropertyAnimator anim = ViewPropertyAnimator
+				.animate(topView)
+				.translationY(
+						ViewHelper.getTranslationY(topView)
+								+ topView.getHeight()).alpha(0).scaleX(1)
+				.setListener(null).setDuration(200);
 		anim.setListener(new AnimatorListenerAdapter() {
 			@Override
 			public void onAnimationEnd(Animator animation) {
 				removeView(topView);
 				ensureFull();
 				final int count = getChildCount();
-				for(int i = 0 ; i < count ; i++){
+				for (int i = 0; i < count; i++) {
 					final View view = getChildAt(i);
-					float scaleX = ViewHelper.getScaleX(view) + ((float)1/mMaxVisible)*0.2f;
-					float tranlateY = ViewHelper.getTranslationY(view) + itemSpace;
-					if(i == count - 1){
+					float scaleX = ViewHelper.getScaleX(view)
+							+ ((float) 1 / mMaxVisible) * 0.2f;
+					float tranlateY = ViewHelper.getTranslationY(view)
+							+ itemSpace;
+					if (i == count - 1) {
 						bringToTop(view);
-					}else{
-						if((count == mMaxVisible && i != 0) || count < mMaxVisible){
-							ViewPropertyAnimator.animate(view)
-							.translationY(tranlateY)
-							.setInterpolator(new AccelerateInterpolator())
-							.setListener(null)
-							.scaleX(scaleX).setDuration(200);
+					} else {
+						if ((count == mMaxVisible && i != 0)
+								|| count < mMaxVisible) {
+							ViewPropertyAnimator
+									.animate(view)
+									.translationY(tranlateY)
+									.setInterpolator(
+											new AccelerateInterpolator())
+									.setListener(null).scaleX(scaleX)
+									.setDuration(200);
 						}
 					}
 				}
@@ -195,55 +238,54 @@ public class CardView extends FrameLayout{
 		});
 		return true;
 	}
-	
+
 	/**
 	 * 将下一个视图移到前边
+	 * 
 	 * @param view
 	 */
 	private void bringToTop(final View view) {
-		float scaleX = ViewHelper.getScaleX(view) + ((float)1/mMaxVisible)*0.2f;
+		float scaleX = ViewHelper.getScaleX(view) + ((float) 1 / mMaxVisible)
+				* 0.2f;
 		float tranlateY = ViewHelper.getTranslationY(view) + itemSpace;
-		ViewPropertyAnimator.animate(view)
-		.translationY(tranlateY)
-		.scaleX(scaleX)
-		.setDuration(200)
-		.alpha(1).setInterpolator(new AccelerateInterpolator())
-		.setListener(null)
-		.setListener(new AnimatorListenerAdapter() {
-			public void onAnimationEnd(Animator animation) {
-				topPosition++;
-				remove = false;
-			}
-		});
+		ViewPropertyAnimator.animate(view).translationY(tranlateY)
+				.scaleX(scaleX).setDuration(200).alpha(1)
+				.setInterpolator(new AccelerateInterpolator())
+				.setListener(null).setListener(new AnimatorListenerAdapter() {
+					public void onAnimationEnd(Animator animation) {
+						topPosition++;
+						remove = false;
+					}
+				});
 	}
-	
-	
+
 	@Override
 	public boolean onInterceptTouchEvent(MotionEvent ev) {
 		float currentY = ev.getY();
-		switch(ev.getAction()){
+		switch (ev.getAction()) {
 		case MotionEvent.ACTION_DOWN:
 			downX = ev.getX();
 			downY = ev.getY();
 			break;
 		case MotionEvent.ACTION_MOVE:
 			float distance = currentY - downY;
-			if(distance > mTouchSlop){
+			if (distance > mTouchSlop) {
 				return true;
 			}
 			break;
 		}
-		return super.onInterceptTouchEvent(ev);
+		return false;
 	}
-	
-	public static Rect getHitRect(Rect rect,View child){
+
+	public static Rect getHitRect(Rect rect, View child) {
 		rect.left = child.getLeft();
 		rect.right = child.getRight();
 		rect.top = (int) (child.getTop() + ViewHelper.getTranslationY(child));
-		rect.bottom = (int) (child.getBottom() + ViewHelper.getTranslationY(child));
+		rect.bottom = (int) (child.getBottom() + ViewHelper
+				.getTranslationY(child));
 		return rect;
 	}
-	
+
 	private final DataSetObserver mDataSetObserver = new DataSetObserver() {
 		@Override
 		public void onChanged() {
@@ -255,11 +297,11 @@ public class CardView extends FrameLayout{
 			super.onInvalidated();
 		}
 	};
-	
+
 	private OnClickListener listener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			if(mListener != null){
+			if (mListener != null) {
 				mListener.onCardClick(v, topPosition);
 			}
 		}
